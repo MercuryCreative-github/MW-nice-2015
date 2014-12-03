2fdd13ec45423a080eadb21d33afa733fb7cc64b
Miguel Vallvé <mvallve@ameba.com.uy>
2014-12-03 19:46:57 -0200

    js

--- themes/tmfmalaysia/js/tmf-main.js
+++ themes/tmfmalaysia/js/tmf-main.js
@@ -9,11 +9,25 @@ jQuery(document).ready(function($) {
 
     resizeBoxex(jQuery(window).width());
 
+    //menu open/close
+    var menu_flag = false; 
+    
+    jQuery( ".navbar-toggle" ).click(
+      function() {
+      if(menu_flag == false)
+        {
+          jQuery( ".navbar-collapse" ).slideDown();
+          menu_flag = true;
+        }
+      else
+        {
+          jQuery( ".navbar-collapse" ).slideUp();
+          menu_flag = false;
+        } 
+      }
+    );
 
-
-
-
-
+    menuChanges();
 });
 
 jQuery(window).resize(function() {
@@ -37,10 +51,65 @@ function resizeBoxex(windowWidth) {
     jQuery('.third-section .box').css('height', boxHeight);
 
 
-	//jQuery('style').append('.third-section .box{height:'+boxHeight+'px!important;}');
+    //jQuery('style').append('.third-section .box{height:'+boxHeight+'px!important;}');
 
 
     /*if(jQuery(window).width()<768){windowWidth=jQuery(window).width()}
     if(windowWidth<768 && windowWidth<jQuery(window).width()){jQuery(window).reload();}*/
 
 }
+
+function menuChanges(){
+    mainnav=jQuery('#navigationmain').addClass('originalNav').css('margin',0).html();
+    jQuery('.head .twelve.columns.nav-border').prepend('<nav id="navigationmain" class="secondNav">'+mainnav+'</nav>');
+
+
+    jQuery('.secondNav ul').css({'position':'absolute', 'right':0});
+    jQuery('.secondNav ul li').each(function(){
+        if(!jQuery(this).hasClass('social-icon')){jQuery(this).remove();}
+    })
+    jQuery('.originalNav ul li').each(function(){
+        if(jQuery(this).hasClass('social-icon')){jQuery(this).remove();}
+    })
+
+    //jQuery('.secondNav select').remove();
+
+    //change social icons
+    jQuery('.icon-linkedin-sign').removeClass('icon-linkedin-sign').addClass('icon-linkedin');
+    jQuery('.icon-facebook-sign').removeClass('icon-facebook-sign').addClass('icon-facebook');
+    jQuery('.icon-twitter-sign').removeClass('icon-twitter-sign').addClass('icon-twitter');
+    jQuery('.icon-google-plus-sign').removeClass('icon-google-plus-sign').addClass('icon-google-plus');
+
+    //call to action remove <li>, move class to <a>
+    var classes = jQuery('#menu-item-2463').attr('class'); //keep updated
+    jQuery('#menu-item-2463 a').addClass(classes);
+    //remove <li>
+    jQuery('#menu-item-2463 a').unwrap();
+
+    //add clasess to updated footer button
+    jQuery('#updated').addClass(classes);
+
+    var classes = jQuery('#menu-item-2464').attr('class'); // register
+    jQuery('#menu-item-2464 a').addClass(classes);
+    jQuery('#menu-item-2464 a').unwrap();
+
+    //add clasess to register footer button
+    jQuery('#register').addClass(classes);
+
+    //get register link for mobile and footer button
+    var a_href = jQuery('.register-btn').attr('href');
+    jQuery('.register-btn-mobile').attr('href',a_href);
+    jQuery('#register').attr('href',a_href);
+
+    //get keep updated button link for footer button
+    var a_href = jQuery('.keep-updated-btn').attr('href');
+    jQuery('#updated').attr('href',a_href);
+
+
+}
+
+
+
+
+
+
