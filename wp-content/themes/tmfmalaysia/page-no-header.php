da1e724904ce3fbf1c948abb6eb110da8691c510
Ligia <Mazzilli>
2015-02-02 14:00:16 -0200

    no header

---
+++ wp-content/themes/tmfmalaysia/page-no-header.php
@@ -0,0 +1,33 @@
+<?php 
+	/*
+	Template Name: Page Without Title
+	*/
+	get_header();
+	the_post(); 
+	$option = get_option('montreal_theme_options'); 
+?>
+
+<div class="bigpadding" style="background:url(<?php echo $option['background_image']; ?>);"></div>
+
+<div class="container white">
+
+	<section class="row">
+		
+		<div class="five columns alpha centered blackhorizontal"></div>
+		
+		<div class="four columns alpha centered midtopmargin">
+			<p class="center meta">
+				<?php echo get_post_meta( $post->ID, '_cmb_the_subtitle', true ); ?>
+			</p>
+		</div>
+		
+	</section>
+	
+	<section <?php post_class('row bigtoppadding the-content'); ?>>
+		<?php the_content(); ?>
+	</section>
+
+</div>
+
+<?php 
+	get_footer();
