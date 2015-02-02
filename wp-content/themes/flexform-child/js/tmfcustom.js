60378f5a0c153df0eba81ffc29fc3c0e6fc689aa
Miguel Vallvé <mvallve@ameba.com.uy>
2015-02-02 15:35:04 -0200

    tabs function

--- wp-content/themes/flexform-child/js/tmfcustom.js
+++ wp-content/themes/flexform-child/js/tmfcustom.js
@@ -56,7 +56,7 @@ function reRowIotTabs() {
     jQuery('.row:eq(1)', this).removeClass( "row" ).addClass( "row-fluid tabs-summits-content");
     //append newSpan4 to Rowfluid
     jQuery('.row-fluid', this).append(newSpan4);
-     jQuery('.row:eq(1) .span12', this).removeClass( "span12" ).addClass( "span4" );
+    jQuery('.row-fluid .span12', this).removeClass( "span12" ).addClass( "span4" );
 
 
 
