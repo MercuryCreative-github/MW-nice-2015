4add34e2be7f9f058c6ec545851ddeaf88ae318f
Miguel Vallvé <mvallve@ameba.com.uy>
2015-02-11 15:52:20 -0200

    no loading the logos

--- wp-content/themes/flexform-child/js/tmfcustom.js
+++ wp-content/themes/flexform-child/js/tmfcustom.js
@@ -21,7 +21,7 @@ function homepageSectionsResize(){
     var $ = jQuery;
     if($('.page-template-homepage-php').index()>0){
 
-        var sponsorsHeight  = $('.section01-sponsors').outerHeight();
+        var sponsorsHeight  = 108;//$('.section01-sponsors').outerHeight();
         var minHeight       = 760-sponsorsHeight;
         var screenHeight    = Math.max($(window).height(),minHeight);
         var menuHeight      = $('#header-section').outerHeight();
