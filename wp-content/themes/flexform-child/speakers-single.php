edbdf831074fac59dc236172d8a3ff200070fa68
pablo-novoa <novoanpablo@gmail.com>
2015-03-25 17:43:31 -0300

    speakers img fix

--- wp-content/themes/flexform-child/speakers-single.php
+++ wp-content/themes/flexform-child/speakers-single.php
@@ -157,7 +157,7 @@ Template Name: Speaker Profile
 $speakerItem= '<div class="speaker-info">';
 	$speakerItem.= '<div class="thumb-single thumb">'; 
 	$userMetaImage=get_user_meta($user->ID,'image',true);
-		$speakerItem.='<img src="'.$userMetaImage.'"/>';
+		$speakerItem.='<img src="'.$userMetaImage.'" onload="speakerImgSize(this);"/>';
 		$speakerItem.= '<div style="margin-top: 20px;"><a href="'.$companyURL.'" target="_blank">'.$companyThumb.'</a></div>';
 	$speakerItem.= '</div>';
 	$speakerItem.= '<div class="subtitle" style="display:inline-block;vertical-align: top;">';
