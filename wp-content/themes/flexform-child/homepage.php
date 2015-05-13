29fd282541f21ef47c99fab26b6ffb736ab20a77
Ligia <Mazzilli>
2015-05-13 14:03:30 -0300

    remove the tweeter script

--- wp-content/themes/flexform-child/homepage.php
+++ wp-content/themes/flexform-child/homepage.php
@@ -410,12 +410,14 @@ TM FORUM LIVE!</h1>
         <div class="widget-heading clearfix">
         
             <h2 class="tw-title">Latest on Twitter</h2>
-            <script>[CBC country="cn" show="n"]</script><a class="twitter-timeline" width"100%" height="380" href="https://twitter.com/tmforumorg" data-widget-id="365265624051617792" data-chrome="noheader transparent" data-link-color="#338ECC" data-border-color="#ffffff" data-tweet-limit="4" data-src-2x="false" data-src="false" >
+            <script>[CBC country="cn" show="n"]</script>
+            <a class="twitter-timeline" width"100%" height="380" href="https://twitter.com/tmforumorg" data-widget-id="365265624051617792" data-chrome="noheader transparent" data-link-color="#338ECC" data-border-color="#ffffff" data-tweet-limit="4" data-src-2x="false" data-src="false" >
                 Tweets by @tmforumorg
-            </a> <script> [/CBC] </script>
+            </a> 
             <script>
                 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
             </script>
+            <script> [/CBC] </script>
             <div class="clear"></div> 
             
 		</div>
