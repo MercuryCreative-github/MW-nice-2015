c7ed245d2cd8e0d4228c376709671adc025d1be8
Ligia <Mazzilli>
2015-05-29 08:38:02 -0300

    video-7

--- wp-content/themes/flexform-child/homepageattheevent.php
+++ wp-content/themes/flexform-child/homepageattheevent.php
@@ -241,12 +241,12 @@ Template Name: Home Page at the Event
 			include_once( ABSPATH . WPINC . '/feed.php' );
 
 			// Get a SimplePie feed object from the specified feed source.
-			$rss = fetch_feed( 'http://tmforum.staging.wpengine.com/video-feed-6/' );
+			$rss = fetch_feed( 'http://tmforum.staging.wpengine.com/video-feed-7/' );
 
 			if ( ! is_wp_error( $rss ) ) : // Checks that the object is created correctly
 
 			    // Figure out how many total items there are, but limit it to 5. 
-			    $maxitems = $rss->get_item_quantity( 3 ); 
+			    $maxitems = $rss->get_item_quantity( 4 ); 
 
 			    // Build an array of all the items, starting with element 0 (first element).
 			    $rss_items = $rss->get_items( 0, $maxitems );
@@ -623,7 +623,7 @@ Template Name: Home Page at the Event
 			include_once( ABSPATH . WPINC . '/feed.php' );
 
 			// Get a SimplePie feed object from the specified feed source.
-			$rss = fetch_feed( 'http://tmforum.staging.wpengine.com/video-feed-6/' );
+			$rss = fetch_feed( 'http://tmforum.staging.wpengine.com/video-feed-7/' );
 
 			if ( ! is_wp_error( $rss ) ) : // Checks that the object is created correctly
 
